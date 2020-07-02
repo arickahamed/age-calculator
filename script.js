eb0db868684a9acd06e180c0ce81bc1bb98de3fe
arickahamed <arickahamed700@gmail.com>
2020-07-02 17:41:13 +0600

    alhamdulillah done

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ function ageInDays() {
   const birthYear = prompt(
     "Hey, wanna check your age then say what year were you born?"
   );
-  const mainCalculation = (2020 - birthYear) * 365;
-  const h1 = document.createElement("h1");
-  const textAnswer = document.createTextNode(
-    "You are" + mainCalculation + "days old."
-  );
-  h1.setAttribute("id", "ageInDays");
-  h1.appendChild(textAnswer);
-  document.getElementById("flex-box-result").appendChild(h1);
+  if (birthYear > 2020 || birthYear === "") {
+    alert("You have not born yet!");
+  } else {
+    const mainCalculation = (2020 - birthYear) * 365;
+    const h1 = document.createElement("h1");
+    const textAnswer = document.createTextNode(
+      "You are" + mainCalculation + "days old."
+    );
+    h1.setAttribute("id", "ageInDays");
+    h1.appendChild(textAnswer);
+    document.getElementById("flex-box-result").innerHTML = null;
+    document.getElementById("flex-box-result").appendChild(h1);
+  }
 }
 
 function reset() {
